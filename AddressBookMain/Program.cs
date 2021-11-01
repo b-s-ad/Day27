7c41a1a76bee3c91742b428af8f9b94d2a60701b
b-s-ad <ahmedshehbazb@gmail.com>
2021-11-01 02:58:35 -0700

    Welcome to Address Book

---
+++ AddressBookMain/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace AddressBookMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Welcome to Address Book");
+            ReadLine();
+        }
+    }
+}
